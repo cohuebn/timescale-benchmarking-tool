0315c11e0106f1c051b4e6feba7ef504705c2cfc
Cory Huebner <chuebner86@gmail.com>
2024-10-02 22:25:21 -0500

    #4 - split out connection pool setup to simplify main function

--- benchmarking-tool/cmd/benchmarking-tool/main.go
+++ benchmarking-tool/cmd/benchmarking-tool/main.go
@@ -6,11 +6,11 @@ import (
 
 	"github.com/cohuebn/timescale-benchmarking-tool/internal/cli"
 	"github.com/cohuebn/timescale-benchmarking-tool/internal/database"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func main() {
-	cliArguments := cli.ParseCliArguments()
-	
+// Setup a connection pool used by the application using CLI arguments
+func setupConnectionPool(cliArguments cli.CliArguments) *pgxpool.Pool {
 	// Initialize the connection pool to the database
 	connectionString := cli.CreateConnectionString(cliArguments)
 	connectionPool, err := database.CreateConnectionPool(connectionString)
@@ -18,6 +18,14 @@ func main() {
 		// If we can't connect to the database, there's no point in continuing
 		log.Panic(err)
 	}
+	return connectionPool
+}
+
+func main() {
+	cliArguments := cli.ParseCliArguments()
+	
+	// Initialize the connection pool to the database
+	connectionPool := setupConnectionPool(cliArguments)
 	defer connectionPool.Close()
 
 	connectivityCheck := database.RunConnectivityCheck(connectionPool)
