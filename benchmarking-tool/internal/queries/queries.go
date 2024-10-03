00e7f36a38d5913247cac3ebcf0268417b64a112
Cory Huebner <chuebner86@gmail.com>
2024-10-02 23:26:49 -0500

    #6 - finished query; fixed column names

--- benchmarking-tool/internal/queries/queries.go
+++ benchmarking-tool/internal/queries/queries.go
@@ -5,12 +5,12 @@ import "fmt"
 func ConvertCpuUsageParamsToQuery(params CpuUsageQueryParams) string {
 	return fmt.Sprintf(`
 		select
-			hostname,
-			time_bucket('1 minute', time) as time,
+			time_bucket('1 minute', ts) as bucket,
 			max(usage) as max_usage,
-			min(usage) as min_usage,
+			min(usage) as min_usage
 		from cpu_usage
-		where hostname = '%s'
-		and time between '%s' and '%s';
+		where host = '%s'
+		and ts between '%s' and '%s'
+		group by bucket;
 	`, params.Hostname, params.StartTime, params.EndTime)
 }
--- benchmarking-tool/internal/queries/queries_test.go
+++ benchmarking-tool/internal/queries/queries_test.go
@@ -18,6 +18,6 @@ func TestConvertCpuUsageParamsToQuery(t *testing.T) {
 	// Not testing everything here, just the parameter replacement
 	// In a production application, I'd write integration tests to validate the query returns expected results
 	// when run against an actual TimescaleDB instance
-	assert.Contains(t, result, "where hostname = 'host_12345'")
-	assert.Contains(t, result, "and time between '2017-01-02 03:04:05' and '2017-02-03 06:59:59'")
+	assert.Contains(t, result, "where host = 'host_12345'")
+	assert.Contains(t, result, "and ts between '2017-01-02 03:04:05' and '2017-02-03 06:59:59'")
 }
